ba43159532d928e923e2d59acd2542035afa458b
Priyansh Sahu <priyansh.sahu25@gmail.com>
2024-02-06 19:26:03 +0000

    commit

--- basics/01.js
+++ basics/01.js
@@ -1,109 +1,90 @@
-let boxes= document.querySelectorAll('.btn');
-let turn =document.querySelector('#turn');
-let announcement= document.createElement('h3');
-announcement.setAttribute('id','winner_anoncement');
-let body =document.querySelector('body');
-let reset_btn= document.querySelector('.reset');
-let draw_heading=document.createElement('h3')
-let turn0=true;
-const winpattern=[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
+let boxes = document.querySelectorAll(".btn");
+let turn = document.querySelector("#turn");
+let announcement = document.createElement("h3");
+announcement.setAttribute("id", "winner_anoncement");
+let body = document.querySelector("body");
+let reset_btn = document.querySelector(".reset");
+let draw_heading = document.createElement("h3");
+let turn0 = true;
+const winpattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
+let draw_check = false;
 
-
-
-
-
-let draw_check=false;
-
-
-boxes.forEach((box)=>{
-box.addEventListener('click',()=>{
-    if(turn0==true){
-        box.innerText='0';
-        turn.innerText='Turn of X';
-       
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turn0 == true) {
+      box.innerText = "0";
+      turn.innerText = "Turn of X";
+    } else {
+      box.innerText = "X";
+      turn.innerText = "Turn of 0";
     }
-    else  {
-        box.innerText='X';
-        turn.innerText='Turn of 0';
-      
-    }
-    turn0=!turn0;
-    box.disabled=true;
-    
-    draw_check =check_winner();
+    turn0 = !turn0;
+    box.disabled = true;
+
+    draw_check = check_winner();
     match_tied();
-})
+  });
 });
 
+const disable_boxes = () => {
+  for (let box of boxes) box.disabled = true;
+};
 
+const check_winner = () => {
+  for (let winner of winpattern) {
+    let pos1 = boxes[winner[0]].innerText;
+    let pos2 = boxes[winner[1]].innerText;
+    let pos3 = boxes[winner[2]].innerText;
 
-
-
-const disable_boxes=()=>{
-    for(let box of boxes) box.disabled=true;
-}
-
-
-
-const check_winner=()=>{
-    for (let winner of winpattern){
-        let pos1=boxes[winner[0]].innerText;
-        let pos2=boxes[winner[1]].innerText;
-        let pos3=boxes[winner[2]].innerText;
-
-        if(pos1!='' && pos2!='' && pos3!=''){
-            if(pos1 =='0' && pos2 =='0' && pos3 =='0'){
-                announcement.innerText='Winner is 0';
-                body.prepend(announcement);
-                turn.remove();
-                disable_boxes();
-                reset_btn.style.marginTop='20px';
-                return true;
-            }
-            else if(pos1 =='X' && pos2 =='X' && pos3 =='X'){
-                announcement.innerText='Winner is X';
-                body.prepend(announcement);
-                turn.remove();
-                disable_boxes();
-                reset_btn.style.marginTop='20px';
-                return true;
-            }
-        }
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 == "0" && pos2 == "0" && pos3 == "0") {
+        announcement.innerText = "Winner is 0";
+        body.prepend(announcement);
+        turn.remove();
+        disable_boxes();
+        reset_btn.style.marginTop = "20px";
+        return true;
+      } else if (pos1 == "X" && pos2 == "X" && pos3 == "X") {
+        announcement.innerText = "Winner is X";
+        body.prepend(announcement);
+        turn.remove();
+        disable_boxes();
+        reset_btn.style.marginTop = "20px";
+        return true;
+      }
     }
- 
-}
+  }
+};
 
-
-
-reset_btn.addEventListener('click',()=>{
-for(let box of boxes){
-    box.innerText='';
+reset_btn.addEventListener("click", () => {
+  for (let box of boxes) {
+    box.innerText = "";
     box.disabled = false;
-    turn.innerText='Turn of 0';
-    document.querySelector('div').insertAdjacentElement('afterend', turn);
+    turn.innerText = "Turn of 0";
+    document.querySelector("div").insertAdjacentElement("afterend", turn);
     announcement.remove();
-}
+  }
 });
 
 const match_tied = () => {
-    let totalMoves = 0;
-    for (let box of boxes) {
-        if (box.innerText !== '') {
-            totalMoves++;
-        }
+  let totalMoves = 0;
+  for (let box of boxes) {
+    if (box.innerText !== "") {
+      totalMoves++;
     }
-   
-    if (totalMoves === 9 && !draw_check) {
-       turn.innerText='The game is drawn.'
-    }
-}
+  }
+
+  if (totalMoves === 9 && !draw_check) {
+    turn.innerText = "The game is drawn.";
+  }
+};
