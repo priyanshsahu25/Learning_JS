c7f45799346a7468358c384fdec88ad12ea07253
Priyansh Sahu <priyansh.sahu25@gmail.com>
2024-02-06 16:12:24 +0000

    commit

--- basics/01.js
+++ basics/01.js
@@ -19,9 +19,28 @@ box.addEventListener('click',()=>{
     else  box.innerText='X';
     turn0=!turn0;
     box.disabled=true;
+    check_winner();
 })
 });
 
-for (let winner of winpattern){
-    console.log(winner);
+let announcement= document.createElement('h3');
+let body =document.querySelector('body');
+const check_winner=()=>{
+    for (let winner of winpattern){
+        let pos1=boxes[winner[0]].innerText;
+        let pos2=boxes[winner[1]].innerText;
+        let pos3=boxes[winner[2]].innerText;
+
+        if(pos1!='' && pos2!='' && pos3!=''){
+            if(pos1 =='0' && pos2 =='0' && pos3 =='0'){
+                announcement.innerText='Winner is 0';
+                body.append(announcement);
+            }
+            else if(pos1 =='X' && pos2 =='X' && pos3 =='X'){
+                announcement.innerText='Winner is X';
+                body.append(announcement);
+            }
+        }
+    }
 }
+
